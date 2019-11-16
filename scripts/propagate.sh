08774f09827beaab4863867fb2682635d4cb3f53
Marc <marcsdeveloperemail@gmail.com>
2019-11-16 23:13:26 +0000

    Add .minttyrc for Git Bash for Windows

--- scripts/propagate.sh
+++ scripts/propagate.sh
@@ -1,5 +1,15 @@
+#Name of the directory one level up
 mainDir=$(dirname "$(pwd)")
+
+#Bash
 cp "$mainDir"/.bash_profile ~
 cp "$mainDir"/.bashrc ~
+
+#Vim
 cp "$mainDir"/.vimrc ~
+
+#Git
 cp "$mainDir"/.gitconfig ~
+
+#Git Bash for Windows
+cp "$mainDir"/.minttyrc ~
