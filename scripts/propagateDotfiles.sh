d96386386323b93ceb5bb9b458befc45a3efc470
Marc Donald <dev@marcdonald.com>
2019-12-16 23:26:48 +0000

    Update scripts to update and propagate dotfiles

---
+++ scripts/propagateDotfiles.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#Bash
+cp .bash_profile ~
+cp .bashrc ~
+
+#Vim
+cp .vimrc ~
+
+#Git
+cp .gitconfig ~
+
+#Atom
+cp atom/keymap.cson ~/.atom/keymap.cson
+cp atom/config.cson ~/.atom/config.cson
+
+#Shellcheck
+cp .shellcheckrc ~
+
+#Git Bash for Windows
+cp .minttyrc ~
