c0f740a651daad284b8cb084badbebb665182f94
Marc <marcsdeveloperemail@gmail.com>
2019-12-16 23:26:48 +0000

    Update scripts to update and propagate dotfiles

---
+++ scripts/updateDotfiles.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#Bash
+cp ~/.bash_profile .
+cp ~/.bashrc .
+
+#Vim
+cp ~/.vimrc .
+
+#Git
+cp ~/.gitconfig .
+
+#Git Bash for Windows
+cp ~/.minttyrc .
+
+#Atom
+cp ~/.atom/keymap.cson ./atom
+cp ~/.atom/config.cson ./atom
+
+#Shellcheck
+cp ~/.shellcheckrc .
+
+#Do Git Commit
+if [ $# -eq 0 ]; then
+	git st
+	echo "No message given, not committing"
+else
+	git st
+	git add .
+	git cm -m "$1"
+fi
