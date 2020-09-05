bcded7576d2a7b1b3a1cf5e123d54618fea4c1e4
Marc Donald <dev@marcdonald.com>
2020-09-05 22:33:04 +0100

    Disable some polybar modules on vertical monitor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,15 +6,9 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload marc &
-  done
-else
-  polybar --reload marc &
-fi
-
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar marc &
+MONITOR="DP-0" polybar --reload main &
+MONITOR="DP-4" polybar --reload secondary &
+MONITOR="DVI-I-0" polybar --reload vertical &
 
 echo "Polybar launched..."
