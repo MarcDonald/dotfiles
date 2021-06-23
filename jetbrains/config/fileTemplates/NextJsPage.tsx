8d7c6f18f407ed4d344c63d055eaed4a913407a7
Marc Donald <marcsdeveloperemail@gmail.com>
2021-06-23 16:58:17 +0100

    Add additional JetBrains file templates

---
+++ jetbrains/config/fileTemplates/NextJsPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function ${PAGE_NAME}() {
+  return (
+    <>
+      <Head>
+        <title>${PAGE_NAME}</title>
+      </Head>
+      <div>${PAGE_NAME}</div>
+    </>
+  );
+}
