19b419a73cca93eb54035b2855b1a230c980559e
Marc Donald <dev@marcdonald.com>
2023-03-10 02:21:38 +0000

    chore: remove tool-configs in favour of marcdonald/js-config packages

--- .prettierrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	singleQuote: true,
-	tabWidth: 2,
-	useTabs: true,
-	semi: true,
-	trailingComma: 'es5',
-};
--- tool-configs/typescript.lint-staged.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	'*.{js,ts,tsx,jsx}': [
-		'eslint --fix -c .eslintrc.js --ext',
-		'prettier -c .prettierrc.js --write',
-	],
-	'*.{yml,md,json,scss}': ['prettier -c .prettierrc.js --write'],
-};
